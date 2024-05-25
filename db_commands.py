d3238bad51c2b54efba105f86657ec19b42529a2
hikemalliday <117792777+hikemalliday@users.noreply.github.com>
2024-05-25 15:30:12 -0500

    workflow test

--- db_commands.py
+++ db_commands.py
@@ -120,6 +120,7 @@ def delete_mob_death_by_id(mob_death: int):
         return str(e)
      finally:
         conn.close()
+        
 # NOTE: Used to help add_mob_death
 async def fetch_mob_names_master(mob_name: str):
     try:
